06c03edeeec4712f72495f15722b1532b574b5c9
Le Chieu Long <lechieulong2016@gmail.com>
2022-08-21 13:34:39 +0700

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { ACTION } from "./App.js";
+export default function Todo({ todo, dispatch }) {
+  return (
+    <div>
+      <span style={{ color: todo.complete ? "#AAA" : "#000" }}>
+        {todo.name}
+      </span>
+      <button
+        onClick={() =>
+          dispatch({ type: ACTION.TOGGLE_TODO, payload: { id: todo.id } })
+        }
+      >
+        Toggle
+      </button>
+      <button
+        onClick={() =>
+          dispatch({ type: ACTION.DELETE_TODO, payload: { id: todo.id } })
+        }
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
+  
